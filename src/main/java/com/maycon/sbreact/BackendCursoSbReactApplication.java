ae37d90d66ff0bf5468e64733c9c7a1ef718a932
Maycon J Pereira <mayconjovan@gmail.com>
2020-11-11 14:43:16 -0300

    Initialize project

---
+++ src/main/java/com/maycon/sbreact/BackendCursoSbReactApplication.java
@@ -0,0 +1,13 @@
+package com.maycon.sbreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCursoSbReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCursoSbReactApplication.class, args);
+	}
+
+}
