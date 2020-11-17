66a06f8028b590a571162c1ea15302298205d885
Maycon J Pereira <mayconjovan@gmail.com>
2020-11-16 23:26:14 -0300

    Testes de integração de usuario

--- src/test/java/com/maycon/sbreact/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/maycon/sbreact/model/repository/UsuarioRepositoryTest.java
@@ -1,52 +1,96 @@
 package com.maycon.sbreact.model.repository;
 
+import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.maycon.sbreact.model.entity.Usuario;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioRepositoryTest {
-	
+
 	@Autowired
 	UsuarioRepository repository;
-	
-	
+
+	@Autowired
+	TestEntityManager entityManager;
+
 	@Test
 	@DisplayName("Deve retornar verdadeiro quando houver a existencia de um email informado.")
 	public void existsEmail() {
 		// cenário
-		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-		repository.save(usuario);
-		
+		Usuario usuario = criarUsuario();
+		entityManager.persist(usuario);
+
 		// ação/execução
 		boolean result = repository.existsByEmail("usuario@email.com");
-		
-		// verificação		
+
+		// verificação
 		Assertions.assertThat(result).isTrue();
-		
+
 	}
-	
+
 	@Test
 	@DisplayName("Deve retornar falso quando não houver usuario cadastrado com o email informado.")
 	public void notExistsEmail() {
-		// cenario
-		repository.deleteAll();
-		
+		// cenário
+
 		// ação
 		boolean result = repository.existsByEmail("usuario@email.com");
-		//verificação
-		
+		// verificação
+
 		Assertions.assertThat(result).isFalse();
-		
+
 	}
+
+	@Test
+	@DisplayName("Deve persistir um usuario na base de dados.")
+	public void saveUser() {
+		// acao
+		Usuario usuario = criarUsuario();
+		Usuario usuarioSalvo = repository.save(usuario);
+
+		// verificacao
+		Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
+
+	}
+
+	@Test
+	@DisplayName("Deve buscar um usuário por email")
+	public void findByEmail() {
+		Usuario usuario = criarUsuario();
+		entityManager.persist(usuario);
+
+		// verificacao
+		Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+		Assertions.assertThat(result.isPresent()).isTrue();
+
+	}
+
+	public static Usuario criarUsuario() {
+		return Usuario.builder().nome("usuario").email("usuario@email.com").senha("senha").build();
+	}
+	
+	@Test
+	@DisplayName("Deve retornar vazio ao buscar usuario não encontrado na base.")
+	public void findByEmailEmpty() {
+
+		// verificacao
+		Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+		Assertions.assertThat(result.isPresent()).isFalse();
+	}
+
 }
