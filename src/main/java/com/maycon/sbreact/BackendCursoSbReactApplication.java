4352ef171b7222cf499943c9cc97851c46b23f0a
Maycon J Pereira <mayconjovan@gmail.com>
2020-11-25 23:13:35 -0300

    Added getEntries method

--- src/main/java/com/maycon/sbreact/BackendCursoSbReactApplication.java
+++ src/main/java/com/maycon/sbreact/BackendCursoSbReactApplication.java
@@ -2,10 +2,19 @@ package com.maycon.sbreact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BackendCursoSbReactApplication {
+@EnableWebMvc
+public class BackendCursoSbReactApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {		
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendCursoSbReactApplication.class, args);
 	}
--- src/main/java/com/maycon/sbreact/api/resource/LancamentoResource.java
+++ src/main/java/com/maycon/sbreact/api/resource/LancamentoResource.java
@@ -57,6 +57,14 @@ public class LancamentoResource {
 		return ResponseEntity.ok(lancamentos);
 	}
 
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento(@PathVariable("id") Long id) {
+		return service.obterPorId(id)
+				.map(lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+				.orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}	
+	
+	
 	@PostMapping
 	public ResponseEntity salvar(@RequestBody LancamentoDTO dto) {
 		try {
@@ -106,6 +114,19 @@ public class LancamentoResource {
 			service.deletarLancamento(entidade);
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}).orElseGet(() -> new ResponseEntity("Lancamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
+	}
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+				.id(lancamento.getId())
+				.descricao(lancamento.getDescricao())
+				.valor(lancamento.getValor())
+				.mes(lancamento.getMes())
+				.ano(lancamento.getAno())
+				.status(lancamento.getStatus().name())
+				.tipo(lancamento.getTipo().name())
+				.usuario(lancamento.getUsuario().getId())
+				.build();
 	}
 
 	private Lancamento converter(LancamentoDTO dto) {
